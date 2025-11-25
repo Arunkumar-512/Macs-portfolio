1da449991bead133b58834cf33baa87235cc6035
ArunKumar-512 <arunkumar283512@gmail.com>
2025-11-25 13:50:19 +0530

    add responsive navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Navbar from '#components/NavBar'
 import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-blue-200'>
-      <h1 className='text-gray-600'>welcome to the macs portfolio app</h1>
-    </div>
+    <main>
+      <Navbar />
+      
+    </main>
   )
 }
 
