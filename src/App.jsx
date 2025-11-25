d68097c46b5a70ee7a072e65c26a066e1139fc44
ArunKumar-512 <arunkumar283512@gmail.com>
2025-11-25 13:04:45 +0530

    Initial commit: Setup project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-blue-200'>
+      <h1 className='text-gray-600'>welcome to the macs portfolio app</h1>
+    </div>
+  )
+}
+
+export default App
